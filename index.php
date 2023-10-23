d0b2ba64b56e070b0468879daf4a5fd871a61d51
Aika Daniel Antunes de Melo e Silva <141881259+aikadaniel@users.noreply.github.com>
2023-10-23 17:38:06 -0300

    update 10/23/2023

--- index.php
+++ index.php
@@ -4,9 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Homepage</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
-    
+    <div class="container overflow-hidden justify-content-center flex-column"> <!--Configuração do espaçamento e alinhamento dos conteudos   -->
+    <header>
+        <div class="text-center fs-1 col-12">Bem Vindo</div> <!-- Configuração do texto -->
+    </header>
+<main>
+    <div class="container justify-content-center text-center col-12 p-12"> <!-- Configuração do botão que ira levar até a pagina de jogos -->
+        <button name="btnGames" type="button" class = "btn btn-primary">Jogos</button> <!-- Botao que ira levar até a pagina de jogos -->
+    </div>
+    </div>
+</main>
 </body>
 </html>
